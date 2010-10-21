064e49b81b38e944b34f5d4ac2a25acfacce7d6c
Rohan Deshpande <rohan.deshpande@gmail.com>
2010-10-20 23:08:25 -0400

    fix bugs

--- gigabitly.rb
+++ gigabitly.rb
@@ -2,13 +2,14 @@
 
 require 'rubygems'
 require 'bundler/setup'
-require 'bitly'
 require 'sinatra'
 require 'erb'
 require 'cgi'
 require 'json'
 require 'mechanize'
 
+require 'bitly'
+Bitly.use_api_version_3
 module Lookup
   def self.keywords(query)
     words = []
@@ -27,7 +28,7 @@ module Lookup
         top_five_tags = hash.first['top_tags'].sort {|a,b| b[1] <=> a[1] }.first(5).map(&:first)
       end
 
-      top_five_tags
+      top_five_tags.select { |word| Registration.keyword_available?(word) }
     end
 
     def self.http_get(url)
@@ -50,29 +51,28 @@ module Lookup
 end
 
 module Registration
-  
-  def self.init
-    config_file = File.join(File.dirname(__FILE__), 'bitly.yml')
-    if !File.file?(config_file)
-      raise "ERROR: Must have a bitly.yml file (see bitly.yml.example)"
-    end
 
-    config = YAML.load(config_file)['bitly']
-    Bitly.use_api_version_3
-    @@bitly = Bitly.new(config['username'], config['api_key'])
+  def self.config
+    @@config ||= begin
+      config_file = File.join(File.dirname(__FILE__), 'bitly.yml')
+      raise "ERROR: Must have a bitly.yml file (see bitly.yml.example)" if !File.file?(config_file)
+      YAML.load_file(config_file)['bitly']
+    end
+  end
+
+  def self.bitly
+    @@bitly ||= Bitly.new(config['username'], config['api_key'])
   end
 
   def self.keyword_available?(keyword)
-    info = @@bitly.info(keyword)
-    info.error && info.error == "NOT_FOUND"
+    info = bitly.info(keyword)
+    info.respond_to?(:error) && info.error == "NOT_FOUND"
   end
 
   def self.register(url, short)
     @@bitly.shorten(url, short)
   end
 end
-
-Registration.init
 
 get '/' do
   @title = "Gigabitly"
